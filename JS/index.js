2a375918c07e130d2c2aad81c7ba09b757c570ad
JuliusWon <71460934+JuliusWon@users.noreply.github.com>
2021-10-18 09:47:38 -0400

    Added files

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+function OpenStackOverflow(){
+    window.open("https://stackoverflow.com/")
+}
+function Openbugfixer(){
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
+function OpenGithub(){
+    window.open("https://github.com/")
+}
+function OpenURL(URL){
+    window.open(URL);
+}
+function spamAlerts(numAlerts){
+    for(i = 0; i<numAlerts*10;i++){
+        alert("You were warned, "+(numAlerts*10-i)+" alerts left");
+    }
+}
+function myFunction(){
+	alert(navigator.userAgent+navigator.maxTouchPoints)
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# LACS_Programming_Project
+# LACS_Programming_Makerspace
+The source code for the LACS makerspace website.
+This is a cool demo I made for the Library Committee. It can provide students with some useful sources to *git* started.
+
+Students are welcome to use any of this code in their own projects. To use the 
